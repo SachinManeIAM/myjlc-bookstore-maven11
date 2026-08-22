814fe9ffa56471f5faeb1cb8cea32547f543c443
Sachin Mane <sachinmane9673@gmail.com>
2026-08-23 02:23:08 +0530

    all jobs are updated

--- src/main/java/com/jlcindia/springboot/MyBootApplication.java
+++ src/main/java/com/jlcindia/springboot/MyBootApplication.java
@@ -10,7 +10,7 @@ System.out.println("Starting Boot Application");
 System.out.println("Update -1 Srinivas");
   System.out.println("Update -2 by Srinivas");
   System.out.println("Update -3 by Sachin Mane");
-
+  System.out.println("Successfully completed all 5 jobs");
 SpringApplication.run(MyBootApplication.class, args);
 }
 }
